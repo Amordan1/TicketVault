d382c8aae174236c2d969fec5001287466d3061f
Reza Majlessi <rm634127@southeast.edu>
2022-09-11 18:03:47 -0500

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TextInputComponent, TouchableOpacity, View } from 'react-native';
+import concert from './assets/concert.png'
+import * as SplashScreen from 'expo-splash-screen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [total, setTotal] = useState();
+  const [show, setShow] = useState(false);
+
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync, 2000)
+
+  function onChange(event) {
+     setTotal((event * 99.99).toFixed(2))
+     setShow(false)
+  };
+    return(
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Ticket Vault</Text>
+        <TextInput style={styles.input} onChangeText={value => onChange(value)} placeholder='Number of Tickets'></TextInput>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={()=>setShow(true)}
+        >
+          <Text style={styles.buttonText}>Find the Cost</Text>
+        </TouchableOpacity>
+        {
+          show?
+          <Text style={styles.returnText}>Ticket Cost: ${total}</Text>
+          :<Text style={styles.returnText}></Text>
+        }
+        <Image source={concert}></Image>
+        <StatusBar style="auto" />
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +43,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  titleText: {
+    fontSize: 50,
+    fontWeight: 'bold'
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 10,
+    borderColor: "grey",
+    borderWidth: 1,
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 5
+  },
+  button: {
+    backgroundColor: "#f98b88",
+    height: 45,
+    paddingHorizontal: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  returnText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    margin: 45
+  }
+})
+
+export default App;
